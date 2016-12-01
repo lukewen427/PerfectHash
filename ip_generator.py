7201c9d89f767d676780ab35a021fecbd4c5af1a
lukewen427 <wenluke427@gmail.com>
2016-12-01 19:01:39 +0000

    new

--- ip_generator.py
+++ ip_generator.py
@@ -1,4 +1,4 @@
-import random
+import numpy as np, numpy.random
 
 def ip_generator(total_num):
     init = "10.10.10"
@@ -11,12 +11,28 @@ def ip_generator(total_num):
     while (i < total_num):
         end = ""
         for a in range(4-l):
-            end += "."+str(random.randint(0, 256))
+            end += "."+str(np.random.randint(0, 256))
         ip_addres = init + end
         if ip_addres not in ip_table:
             ip_table.append(ip_addres)
             i += 1
     return ip_table
 
+
+def dataCenter_ip_generator(total_num, block_num):
+    init = "10.10"
+    ip_table = []
+    blocks = np.random.dirichlet(np.ones(block_num),size=1)
+    for i in range (block_num):
+        print i
+        block_id = "."+str(np.random.randint(0, 256))
+        for a in range(int(i*total_num)):
+            ip_addres = init+block_id+"."+str(np.random.randint(0, 256))
+            print ip_addres
+            if ip_addres not in ip_table:
+                ip_table.append(ip_addres)
+    return ip_table
+
 if __name__ == "__main__":
-    ip_generator(10)
+    # ip_generator(10)
+    dataCenter_ip_generator(10, 1)
